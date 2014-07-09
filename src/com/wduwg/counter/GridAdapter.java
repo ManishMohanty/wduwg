07c044e136d597278c2cc08ae6eb115a992190cd
PrabhanshuShekhar <prabhanshu@motifworks.com>
2014-07-09 17:39:28 +0530

    change in grid adapter

--- src/com/wduwg/counter/GridAdapter.java
+++ src/com/wduwg/counter/GridAdapter.java
@@ -42,10 +42,18 @@ public class GridAdapter extends BaseAdapter{
 		return 0;
 	}
 
+	
+	static class ViewHolder {
+		protected SmartImageView imageView;
+		protected TextView businessName;
+		protected TextView address;
+	}
+	
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		// TODO Auto-generated method stub
 		Business tempPlace = (Business)businessList.get(position);
+		final ViewHolder viewHolder;
 		LayoutInflater inflater = (LayoutInflater) context
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
@@ -54,57 +62,54 @@ public class GridAdapter extends BaseAdapter{
 			if (convertView == null) {
 
 				gridView = new View(context);
-
+				viewHolder = new ViewHolder();
 				// get layout from mobile.xml
-				gridView = inflater.inflate(R.layout.singlegrid, null);
+				convertView = inflater.inflate(R.layout.singlegrid,null);
 
 				// set value into textview
-				TextView businessName = (TextView) gridView
+				viewHolder.businessName = (TextView) convertView
 						.findViewById(R.id.businessName);
-				businessName.setText(businessList.get(position).getName());
-
-				TextView address = (TextView)gridView.findViewById(R.id.address);
-				String completeAddress = businessList.get(position).getAddress();
-				if(completeAddress.length() > 20)
-				{
-					completeAddress = completeAddress.substring(0, 20);
-					completeAddress = completeAddress.substring(0,completeAddress.lastIndexOf(' ')) + "...";
-					address.setText(completeAddress);
-				}else
-				{
-				address.setText(businessList.get(position).getAddress());
-				}
-
-//				// set image based on selected text
-				SmartImageView imageView = (SmartImageView) gridView
+				viewHolder.address = (TextView)convertView.findViewById(R.id.address);
+				viewHolder.imageView = (SmartImageView) convertView
 						.findViewById(R.id.image);
-//	            imageView.setImageResource(mThumbIds[4]);
-
-	            try{
-	        		String googleApiResults = tempPlace.getGoogleAPIResult();
-	        		JSONObject json = new JSONObject(googleApiResults);
-
-	        		JSONObject photoJsonObject  = new JSONObject(json.getString("photos").substring(1, json.getString("photos").length()-1).toString());
-	        		tempPlace.setImageUrl(photoJsonObject.getString("photo_reference"));
-	        		System.out.println(">>>>>>> ImageUrl->"+tempPlace.getImageUrl());
-	        		}catch(Exception e)
-	        		{
-	        			e.printStackTrace();
-	        		}
-
-	        		String temp = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=60&photoreference="
-	        				+ tempPlace.getImageUrl()
-	        				+ "&sensor=true&key=AIzaSyBG7NMHOOu50N3C96ZRmR2hgwAmx0KhddI";
-
-	        		System.out.println(">>>>>>> ImageUrl->"+temp);
-
-	        		imageView.setImageUrl(temp);
+                convertView.setTag(viewHolder);
 
 			} else {
-				gridView = (View) convertView;
+				viewHolder = (ViewHolder) convertView.getTag();
 			}
+			viewHolder.businessName.setText(businessList.get(position).getName());
+			String completeAddress = businessList.get(position).getAddress();
+			if(completeAddress.length() > 20)
+			{
+				completeAddress = completeAddress.substring(0, 20);
+				completeAddress = completeAddress.substring(0,completeAddress.lastIndexOf(' ')) + "...";
+				viewHolder.address.setText(completeAddress);
+			}else
+			{
+				viewHolder.address.setText(businessList.get(position).getAddress());
+			}
+			
+			try{
+				String googleApiResults = tempPlace.getGoogleAPIResult();
+				JSONObject json = new JSONObject(googleApiResults);
+				
+				JSONObject photoJsonObject  = new JSONObject(json.getString("photos").substring(1, json.getString("photos").length()-1).toString());
+				tempPlace.setImageUrl(photoJsonObject.getString("photo_reference"));
+				System.out.println(">>>>>>> ImageUrl->"+tempPlace.getImageUrl());
+			}catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+			
+			String temp = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=60&photoreference="
+					+ tempPlace.getImageUrl()
+					+ "&sensor=true&key=AIzaSyBG7NMHOOu50N3C96ZRmR2hgwAmx0KhddI";
+			
+			System.out.println(">>>>>>> ImageUrl->"+temp);
+			
+			viewHolder.imageView.setImageUrl(temp);
 
-			return gridView;
+			return convertView;
 	}
 	
 
