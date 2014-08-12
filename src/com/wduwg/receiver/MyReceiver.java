e357985c285e06a67193f2f602e2b847f01f3e79
PrabhanshuShekhar <prabhanshu@motifworks.com>
2014-08-12 20:09:00 +0530

    change for power issue

--- src/com/wduwg/receiver/MyReceiver.java
+++ src/com/wduwg/receiver/MyReceiver.java
@@ -1,5 +1,7 @@
 package com.wduwg.receiver;
 
+import com.wduwg.watch.app.CountActivity;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -8,7 +10,8 @@ public class MyReceiver extends BroadcastReceiver {
 
 	public void onReceive(Context context, Intent intent) {
 		// TODO Auto-generated method stub
-		Intent intent1 = new Intent(Intent.ACTION_MAIN);
+		Intent intent1 = new Intent(context,CountActivity.class);
+		intent1.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
 		context.startActivity(intent1);
 	}
 }
